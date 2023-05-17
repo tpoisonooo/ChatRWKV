f93b5c114e517c2a10492534ad868afb8ebb44fc
tpoisonooo <khj.application@aliyun.com>
2023-05-17 14:38:01 +0000

    feat(export): add export onnx

--- RWKV_in_150_lines.py
+++ RWKV_in_150_lines.py
@@ -3,6 +3,7 @@
 ########################################################################################################
 
 import numpy as np
+
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 import types, torch
 from torch.nn import functional as F
@@ -11,7 +12,7 @@ from tokenizers import Tokenizer
 tokenizer = Tokenizer.from_file("20B_tokenizer.json")
 
 args = types.SimpleNamespace()
-args.MODEL_NAME = '/fsx/BlinkDL/HF-MODEL/rwkv-4-pile-430m/RWKV-4-Pile-430M-20220808-8066'
+args.MODEL_NAME = '/models/rwkv-4-pile-430m/RWKV-4-Pile-430M-20220808-8066'
 args.n_layer = 24
 args.n_embd = 1024
 
@@ -23,21 +24,122 @@ TOP_P = 0.85
 
 ########################################################################################################
 
+
+class Encoder(torch.nn.Module):
+    def __init__(self, emb, ln_weight, ln_bias):
+        super().__init__()
+        self.emb = emb
+        self.ln_weight = ln_weight
+        self.ln_bias = ln_bias
+    
+    def forward(self, token):
+        x = self.emb[token]
+        x = F.layer_norm(x, (1024, ), weight=self.ln_weight, bias=self.ln_bias)
+        x= x.flatten()
+        return x
+
+
+class Decoder(torch.nn.Module):
+    def __init__(self, head, ln_weight, ln_bias):
+        super().__init__()
+        self.head = head
+        self.ln_weight = ln_weight
+        self.ln_bias = ln_bias
+    
+    def forward(self, x):
+        x = F.layer_norm(x, (1024, ), weight=self.ln_weight, bias=self.ln_bias)
+        x = self.head @ x
+        return x.flatten()
+
+
+class TimeMixer(torch.nn.Module):
+    def __init__(self, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow, ln_weight, ln_bias):
+        super().__init__()
+        self.time_mix_k = time_mix_k
+        self.time_mix_v = time_mix_v
+        self.time_mix_r = time_mix_r
+        self.time_first = time_first
+        self.time_decay = time_decay
+
+        self.kw = kw
+        self.vw = vw
+        self.rw = rw
+        self.ow = ow
+
+        self.ln_weight = ln_weight
+        self.ln_bias = ln_bias
+    
+    def forward(self, x, state, i):
+        y = x
+
+        x = F.layer_norm(x, (1024, ), weight=self.ln_weight, bias=self.ln_bias)
+
+        xk = x * self.time_mix_k + state[5 * i + 1].flatten() * (1 - self.time_mix_k)
+        xv = x * self.time_mix_v + state[5 * i + 1].flatten() * (1 - self.time_mix_v)
+        xr = x * self.time_mix_r + state[5 * i + 1].flatten() * (1 - self.time_mix_r)
+        state[5 * i + 1] = x
+        r = torch.sigmoid(self.rw @ xr)
+        k = self.kw @ xk
+        v = self.vw @ xv
+
+        aa = state[5 * i + 2].flatten()
+        bb = state[5 * i + 3].flatten()
+        pp = state[5 * i + 4].flatten()
+        ww = self.time_first + k
+        qq = torch.maximum(pp, ww)
+        e1 = torch.exp(pp - qq)
+        e2 = torch.exp(ww - qq)
+        a = e1 * aa + e2 * v
+        b = e1 * bb + e2
+        wkv = a / b
+        ww = pp + self.time_decay
+        qq = torch.maximum(ww, k)
+        e1 = torch.exp(ww - qq)
+        e2 = torch.exp(k - qq)
+        state[5 * i + 2] = e1 * aa + e2 * v
+        state[5 * i + 3] = e1 * bb + e2
+        state[5 * i + 4] = qq
+        return y + (self.ow @ (r * wkv))
+
+
+class ChannelMixer(torch.nn.Module):
+    def __init__(self, time_mix_k, time_mix_r, kw, vw, rw, ln_weight, ln_bias):
+        super().__init__()
+        self.time_mix_k = time_mix_k
+        self.time_mix_r = time_mix_r
+        self.kw = kw
+        self.vw = vw
+        self.rw = rw
+        self.ln_weight = ln_weight
+        self.ln_bias = ln_bias
+
+    def forward(self, x, state, i):
+        y = x
+        x = F.layer_norm(x, (1024, ), weight=self.ln_weight, bias=self.ln_bias)
+        xk = x * self.time_mix_k + state[5 * i + 0].flatten() * (1 - self.time_mix_k)
+        xr = x * self.time_mix_r + state[5 * i + 0].flatten() * (1 - self.time_mix_r)
+        state[5 * i + 0] = x
+        r = torch.sigmoid(self.rw @ xr)
+        k = torch.square(torch.relu(self.kw @ xk))  # square relu, primer paper
+        return y + (r * (self.vw @ k))
+
+
 class RWKV_RNN(torch.jit.ScriptModule):
+
     def __init__(self, args):
         super().__init__()
         self.args = args
-        self.eval() # set torch to inference mode
-        
+        self.eval()  # set torch to inference mode
+
         w = torch.load(args.MODEL_NAME + '.pth', map_location='cpu')
         for k in w.keys():
-            if      '.time_' in k: w[k] = w[k].squeeze()
-            if '.time_decay' in k: w[k] = -torch.exp(w[k].float()) # the real time decay is like e^{-e^x}
-            else: w[k] = w[k].float() # convert to f32 type
-        
-        self.w = types.SimpleNamespace() # set self.w from w
+            if '.time_' in k: w[k] = w[k].squeeze()
+            if '.time_decay' in k: w[k] = -torch.exp(w[k].float())  # the real time decay is like e^{-e^x}
+            else: w[k] = w[k].float()  # convert to f32 type
+
+        self.w = types.SimpleNamespace()  # set self.w from w
         self.w.blocks = {}
-        for k in w.keys(): # example: "blocks.0.att.time_first" => self.w.blocks[0].att.time_first
+        for k in w.keys():  # example: "blocks.0.att.time_first" => self.w.blocks[0].att.time_first
             parts = k.split('.')
             last = parts.pop()
             here = self.w
@@ -51,31 +153,37 @@ class RWKV_RNN(torch.jit.ScriptModule):
                     here = getattr(here, p)
             setattr(here, last, w[k])
 
+        self.encoder = Encoder(self.w.emb.weight, self.w.blocks[0].ln0.weight, self.w.blocks[0].ln0.bias)
+        self.encoder.eval()
+
+        self.decoder = Decoder(self.w.head.weight, self.w.ln_out.weight, self.w.ln_out.bias)
+        self.decoder.eval()
+
     def layer_norm(self, x, w):
-        return F.layer_norm(x, (self.args.n_embd,), weight=w.weight, bias=w.bias)
+        return F.layer_norm(x, (self.args.n_embd, ), weight=w.weight, bias=w.bias)
 
     @torch.jit.script_method
-    def channel_mixing(self, x, state, i:int, time_mix_k, time_mix_r, kw, vw, rw):
-        xk = x * time_mix_k + state[5*i+0] * (1 - time_mix_k)
-        xr = x * time_mix_r + state[5*i+0] * (1 - time_mix_r)
-        state[5*i+0] = x
+    def channel_mixing(self, x, state, i: int, time_mix_k, time_mix_r, kw, vw, rw):
+        xk = x * time_mix_k + state[5 * i + 0] * (1 - time_mix_k)
+        xr = x * time_mix_r + state[5 * i + 0] * (1 - time_mix_r)
+        state[5 * i + 0] = x
         r = torch.sigmoid(rw @ xr)
-        k = torch.square(torch.relu(kw @ xk)) # square relu, primer paper
+        k = torch.square(torch.relu(kw @ xk))  # square relu, primer paper
         return r * (vw @ k)
 
     @torch.jit.script_method
-    def time_mixing(self, x, state, i:int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
-        xk = x * time_mix_k + state[5*i+1] * (1 - time_mix_k)
-        xv = x * time_mix_v + state[5*i+1] * (1 - time_mix_v)
-        xr = x * time_mix_r + state[5*i+1] * (1 - time_mix_r)
-        state[5*i+1] = x
+    def time_mixing(self, x, state, i: int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow):
+        xk = x * time_mix_k + state[5 * i + 1] * (1 - time_mix_k)
+        xv = x * time_mix_v + state[5 * i + 1] * (1 - time_mix_v)
+        xr = x * time_mix_r + state[5 * i + 1] * (1 - time_mix_r)
+        state[5 * i + 1] = x
         r = torch.sigmoid(rw @ xr)
         k = kw @ xk
         v = vw @ xv
-        
-        aa = state[5*i+2]
-        bb = state[5*i+3]
-        pp = state[5*i+4]
+
+        aa = state[5 * i + 2]
+        bb = state[5 * i + 3]
+        pp = state[5 * i + 4]
         ww = time_first + k
         qq = torch.maximum(pp, ww)
         e1 = torch.exp(pp - qq)
@@ -87,33 +195,73 @@ class RWKV_RNN(torch.jit.ScriptModule):
         qq = torch.maximum(ww, k)
         e1 = torch.exp(ww - qq)
         e2 = torch.exp(k - qq)
-        state[5*i+2] = e1 * aa + e2 * v
-        state[5*i+3] = e1 * bb + e2
-        state[5*i+4] = qq
+        state[5 * i + 2] = e1 * aa + e2 * v
+        state[5 * i + 3] = e1 * bb + e2
+        state[5 * i + 4] = qq
         return ow @ (r * wkv)
 
-    def forward(self, token, state):
+
+    @torch.jit.script_method
+    def encode_export(self, token, emb, ln_weight, ln_bias):
+        x = emb[token]
+        x = F.layer_norm(x, (1024, ), weight=ln_weight, bias=ln_bias)
+        return x.flatten()
+
+
+    @torch.jit.script_method
+    def decode_export(self, x, head, ln_weight, ln_bias):
+        x = F.layer_norm(x, (1024, ), weight=ln_weight, bias=ln_bias)
+        x = head @ x
+        return x.flatten()
+
+    def forward(self, tokenid, state):
         with torch.no_grad():
-            if state == None:
-                state = torch.zeros(self.args.n_layer * 5, self.args.n_embd)
-                for i in range(self.args.n_layer): state[5*i+4] = -1e30 # -infinity
-            
-            x = self.w.emb.weight[token]
-            x = self.layer_norm(x, self.w.blocks[0].ln0)
+
+            # x = self.w.emb.weight[token]
+            # x = self.layer_norm(x, self.w.blocks[0].ln0)
+
+            token = torch.full([1], tokenid, dtype=torch.int32)
+
+            x = self.encoder.forward(token)
+            # onnx_inputs = [token]
+            # onnx_filepath = 'encode.onnx'
+            # onnx_inp_names = ['token']
+            # onnx_out_names = ['feature']
+            # torch.onnx.export(model=self.encoder, args=onnx_inputs, f=onnx_filepath, verbose=False, input_names=onnx_inp_names, output_names=onnx_out_names, opset_version=16)
+
             for i in range(self.args.n_layer):
+                itensor = torch.full([1], i, dtype=torch.int32)
+
                 att = self.w.blocks[i].att
-                x = x + self.time_mixing(self.layer_norm(x, self.w.blocks[i].ln1), state, i, 
-                    att.time_mix_k, att.time_mix_v, att.time_mix_r, att.time_first, att.time_decay, 
-                    att.key.weight, att.value.weight, att.receptance.weight, att.output.weight)
+                ln1 = self.w.blocks[i].ln1
+
+                # import pdb
+                # pdb.set_trace()
+
+                time_mixer = TimeMixer(att.time_mix_k, att.time_mix_v, att.time_mix_r, att.time_first, att.time_decay, att.key.weight, att.value.weight, att.receptance.weight, att.output.weight, ln1.weight, ln1.bias)
+                
+                # onnx_inputs = [x, state, itensor]
+                # onnx_filepath = 'time_mixing_{}.onnx'.format(i)
+                # onnx_inp_names = ['input', 'state', 'i']
+                # onnx_out_names = ['feature']
+                # import pdb
+                # pdb.set_trace()
+                # torch.onnx.export(model=time_mixer, args=[x, state, itensor], f=onnx_filepath, verbose=False, input_names=onnx_inp_names, output_names=onnx_out_names, opset_version=16)
+                x = time_mixer.forward(x, state, itensor)
+
                 ffn = self.w.blocks[i].ffn
-                x = x + self.channel_mixing(self.layer_norm(x, self.w.blocks[i].ln2), state, i, 
-                    ffn.time_mix_k, ffn.time_mix_r, 
-                    ffn.key.weight, ffn.value.weight, ffn.receptance.weight)
-            
-            x = self.w.head.weight @ self.layer_norm(x, self.w.ln_out)
+                ln2 = self.w.blocks[i].ln2
+
+                channel_mixer = ChannelMixer(ffn.time_mix_k, ffn.time_mix_r, ffn.key.weight, ffn.value.weight, ffn.receptance.weight, ln2.weight, ln2.bias)
+                x = channel_mixer.forward(x, state, itensor)
+
+            x = self.decoder.forward(x)
+            # x = self.w.head.weight @ self.layer_norm(x, self.w.ln_out)
             return x.float(), state
 
+
 ##########################################################################################################
+
 
 def sample_logits(out, temperature=1.0, top_p=0.8):
     probs = F.softmax(out, dim=-1).numpy()
@@ -127,13 +275,18 @@ def sample_logits(out, temperature=1.0, top_p=0.8):
     out = np.random.choice(a=len(probs), p=probs)
     return out
 
+
 ########################################################################################################
 
 print(f'\nUsing CPU. Loading {args.MODEL_NAME} ...')
 model = RWKV_RNN(args)
 
 print(f'\nPreprocessing context (slow version. see v2/rwkv/model.py for fast version)')
-init_state = None
+
+init_state = torch.zeros(args.n_layer * 5, args.n_embd)
+for i in range(args.n_layer):
+    init_state[5 * i + 4] = -1e30  # -infinity
+
 for token in tokenizer.encode(context).ids:
     init_out, init_state = model.forward(token, init_state)
 
@@ -146,8 +299,50 @@ for TRIAL in range(NUM_TRIALS):
         token = sample_logits(out, TEMPERATURE, TOP_P)
         all_tokens += [token]
         tmp = tokenizer.decode(all_tokens[out_last:])
-        if '\ufffd' not in tmp: # only print when we have a valid utf-8 string
+        if '\ufffd' not in tmp:  # only print when we have a valid utf-8 string
             print(tmp, end="", flush=True)
             out_last = i + 1
-        out, state = model.forward(token, state)       
+        out, state = model.forward(token, state)
 print('\n')
+
+# @torch.jit.script_method
+# def channel_mixing_export(self, x, state, i: int, time_mix_k, time_mix_r, kw, vw, rw, ln_weight, ln_bias):
+#     x = F.layer_norm(x, (1024, ), weight=ln_weight, bias=ln_bias)
+
+#     xk = x * time_mix_k + state[5 * i + 0] * (1 - time_mix_k)
+#     xr = x * time_mix_r + state[5 * i + 0] * (1 - time_mix_r)
+#     state[5 * i + 0] = x
+#     r = torch.sigmoid(rw @ xr)
+#     k = torch.square(torch.relu(kw @ xk))  # square relu, primer paper
+#     return r * (vw @ k)
+
+# @torch.jit.script_method
+# def time_mixing_export(self, x, state, i: int, time_mix_k, time_mix_v, time_mix_r, time_first, time_decay, kw, vw, rw, ow, ln_weight, ln_bias):
+#     x = F.layer_norm(x, (1024, ), weight=ln_weight, bias=ln_bias)
+
+#     xk = x * time_mix_k + state[5 * i + 1] * (1 - time_mix_k)
+#     xv = x * time_mix_v + state[5 * i + 1] * (1 - time_mix_v)
+#     xr = x * time_mix_r + state[5 * i + 1] * (1 - time_mix_r)
+#     state[5 * i + 1] = x
+#     r = torch.sigmoid(rw @ xr)
+#     k = kw @ xk
+#     v = vw @ xv
+
+#     aa = state[5 * i + 2]
+#     bb = state[5 * i + 3]
+#     pp = state[5 * i + 4]
+#     ww = time_first + k
+#     qq = torch.maximum(pp, ww)
+#     e1 = torch.exp(pp - qq)
+#     e2 = torch.exp(ww - qq)
+#     a = e1 * aa + e2 * v
+#     b = e1 * bb + e2
+#     wkv = a / b
+#     ww = pp + time_decay
+#     qq = torch.maximum(ww, k)
+#     e1 = torch.exp(ww - qq)
+#     e2 = torch.exp(k - qq)
+#     state[5 * i + 2] = e1 * aa + e2 * v
+#     state[5 * i + 3] = e1 * bb + e2
+#     state[5 * i + 4] = qq
+#     return ow @ (r * wkv)
